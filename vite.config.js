c5411d4ea6d831f10cf8816722c85d71e83bf1f8
springleaf <3313252209@qq.com>
2025-09-27 23:36:35 +0800

    feat：添加add-write.png图标

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8181',
+        target: 'http://127.0.0.1:8181',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
